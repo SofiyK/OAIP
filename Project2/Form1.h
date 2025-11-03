6f489144ca181b19a9cd6c5ce8be82fd4cf9a913
SofiyK <sofiy.kalenichenko@gmail.com>
2025-11-03 16:29:21 +0300

    Удалена папка LR29, создан Project2, являющийся лабораторной работой 29

---
+++ Project2/Form1.h
@@ -0,0 +1,197 @@
+#pragma once
+
+namespace Project2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label4;
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(372, 152);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(39, 25);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"R=";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(417, 115);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(200, 62);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Form1::textBox1_KeyPress);
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.125F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(332, 312);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(272, 96);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Посчитать площадь кольца";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 7.874999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(365, 186);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(51, 72);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"r=";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(417, 200);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(200, 62);
+			this->textBox2->TabIndex = 4;
+			this->textBox2->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Form1::textBox2_KeyPress);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(364, 447);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(0, 73);
+			this->label4->TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1010, 666);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		double R, r, s;
+
+		try {
+			R = Convert::ToDouble(textBox1->Text);
+			r = Convert::ToDouble(textBox2->Text);
+			s = 3.14 * (R * R - r * r);
+			label4->Text = s.ToString();
+		}
+		catch (System::FormatException^ ex) {
+			MessageBox::Show(
+				"Нужно ввести исходные данные", "Радиусы окружностей",
+				MessageBoxButtons::OK,
+				MessageBoxIcon::Exclamation);
+			textBox1->Focus();
+		}
+		
+
+	}
+	
+private: System::Void textBox1_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar >= '0') && (e->KeyChar <= '9')) return;
+	if (e->KeyChar == '.') e->KeyChar = ',';
+	if (e->KeyChar == ',') {
+		if (textBox1->Text->IndexOf(',') != -1)
+			e->Handled = true;
+		return;
+	}
+	if (Char::IsControl(e->KeyChar)) {
+		if (e->KeyChar == (char)Keys::Enter)
+			button1->Focus();
+		return;
+	}
+	e->Handled = true;
+}
+private: System::Void textBox2_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar >= '0') && (e->KeyChar <= '9')) return;
+	if (e->KeyChar == '.') e->KeyChar = ',';
+	if (e->KeyChar == ',') {
+		if (textBox2->Text->IndexOf(',') != -1)
+			e->Handled = true;
+		return;
+	}
+	if (Char::IsControl(e->KeyChar)) {
+		if (e->KeyChar == (char)Keys::Enter)
+			button1->Focus();
+		return;
+	}
+	e->Handled = true;
+}
+};
+}
